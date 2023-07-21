849e09dbed14ad2c3e71adde2338f74aa8624d23
rhyslopez-js <rhys.paulino@vanarts.com>
2023-07-20 20:30:00 -0700

    added the trust icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent {
+
+  public display: any
+
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  
+  ngOnInit() {
+      this.getScreenWidth = window.innerWidth;
+      this.getScreenHeight = window.innerHeight;
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    console.log(this.getScreenWidth)
+
+    if(window.innerWidth = 990){
+      this.display = false
+    }
+    else{
+      this.display = true
+    }
+  }
+
 
 }
